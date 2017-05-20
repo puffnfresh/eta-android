67f558c53d19d382f5367a3e9afa3dc8004c9f20
Brian McKenna <bmckenna@atlassian.com>
2017-05-20 21:48:21 +1000

    Add call to Haskell code

--- app/src/main/java/xyz/profunctor/etaandroid/MainActivity.java
+++ app/src/main/java/xyz/profunctor/etaandroid/MainActivity.java
@@ -2,11 +2,13 @@ package xyz.profunctor.etaandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import xyz.profunctor.android.ActivityImpl;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityImpl.startActivity(this);
     }
 }
