41b4fce1f63f1896ef0496a52857fbcf28f298ef
Brian McKenna <bmckenna@atlassian.com>
2017-05-24 21:27:18 +1000

    Shrink the APK file to 209KB

--- app/src/main/java/xyz/profunctor/etaandroid/MainActivity.java
+++ app/src/main/java/xyz/profunctor/etaandroid/MainActivity.java
@@ -1,10 +1,10 @@
 package xyz.profunctor.etaandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import xyz.profunctor.android.ActivityImpl;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
